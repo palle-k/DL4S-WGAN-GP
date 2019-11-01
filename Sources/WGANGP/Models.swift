2d1403c445641fb1d1a1d5506d38da27d8bcb57b
Palle Klewitz <palleklewitz@icloud.com>
2019-11-01 20:00:32 +0100

    revert stuff

--- Sources/WGANGP/Models.swift
+++ Sources/WGANGP/Models.swift
@@ -29,17 +29,17 @@ import DL4S
 let generator = Sequential {
     Dense<Float, CPU>(inputSize: 50 + 10, outputSize: 512)
     BatchNorm<Float, CPU>(inputSize: [512])
-    Relu<Float, CPU>()
+    LeakyRelu<Float, CPU>(leakage: 0.2)
     
     Reshape<Float, CPU>(outputShape: [8, 8, 8])
     
     TransposedConvolution2D<Float, CPU>(inputChannels: 8, outputChannels: 6, kernelSize: (3, 3), padding: 1, stride: 2)
     BatchNorm<Float, CPU>(inputSize: [6, 15, 15])
-    Relu<Float, CPU>()
+    LeakyRelu<Float, CPU>(leakage: 0.2)
     
     TransposedConvolution2D<Float, CPU>(inputChannels: 6, outputChannels: 3, kernelSize: (3, 3), padding: 1, stride: 2)
     BatchNorm<Float, CPU>(inputSize: [3, 29, 29])
-    Relu<Float, CPU>()
+    LeakyRelu<Float, CPU>(leakage: 0.2)
     
     Convolution2D<Float, CPU>(inputChannels: 3, outputChannels: 1, kernelSize: (2, 2), padding: 0, stride: 1)
     Sigmoid<Float, CPU>()
@@ -57,18 +57,20 @@ struct Critic<Element: RandomizableType, Device: DeviceType>: LayerType {
     }
     
     var convolutions = Sequential {
-        Convolution2D<Element, Device>(inputChannels: 1, outputChannels: 6, kernelSize: (3, 3), padding: 3, stride: 2)  // 16x16
-        BatchNorm<Element, Device>(inputSize: [6, 16, 16])
-        LeakyRelu<Element, Device>(leakage: 0.2)
+        Convolution2D<Element, Device>(inputChannels: 1, outputChannels: 6, kernelSize: (3, 3))  // 28x28
+        Relu<Element, Device>()
         
-        Convolution2D<Element, Device>(inputChannels: 6, outputChannels: 12, kernelSize: (3, 3), stride: 2)  // 8x8
-        BatchNorm<Element, Device>(inputSize: [12, 8, 8])
-        LeakyRelu<Element, Device>(leakage: 0.2)
+        MaxPool2D<Element, Device>(windowSize: 2, stride: 2, padding: 0) // 14x14
         
-        Convolution2D<Element, Device>(inputChannels: 12, outputChannels: 16, kernelSize: (3, 3), stride: 2)  // 4x4
-        BatchNorm<Element, Device>(inputSize: [16, 4, 4])
-        LeakyRelu<Element, Device>(leakage: 0.2)
+        Convolution2D<Element, Device>(inputChannels: 6, outputChannels: 12, kernelSize: (3, 3), padding: 2)  // 16x16
+        Relu<Element, Device>()
         
+        MaxPool2D<Element, Device>(windowSize: 2, stride: 2, padding: 0) // 8x8
+        
+        Convolution2D<Element, Device>(inputChannels: 12, outputChannels: 16, kernelSize: (3, 3))  // 8x8
+        Relu<Element, Device>()
+        
+        MaxPool2D<Element, Device>(windowSize: 2, stride: 2, padding: 0) // 4x4
         Flatten<Element, Device>() // 256
     }
     
@@ -76,8 +78,7 @@ struct Critic<Element: RandomizableType, Device: DeviceType>: LayerType {
         Concat<Element, Device>()
         
         Dense<Element, Device>(inputSize: 256 + 10, outputSize: 128)
-        BatchNorm<Element, Device>(inputSize: [128])
-        LeakyRelu<Element, Device>(leakage: 0.2)
+        Relu<Element, Device>()
         
         Dense<Element, Device>(inputSize: 128, outputSize: 1)
     }
